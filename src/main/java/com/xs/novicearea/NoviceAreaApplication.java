d53ee47ab1ab99b468359249a3c6d07a261780e2
lxs <202114366@qq.com>
2023-01-23 19:38:05 +0800

    add:Initial commit

---
+++ src/main/java/com/xs/novicearea/NoviceAreaApplication.java
@@ -0,0 +1,13 @@
+package com.xs.novicearea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoviceAreaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoviceAreaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xs/novicearea/NoviceAreaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xs.novicearea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoviceAreaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
